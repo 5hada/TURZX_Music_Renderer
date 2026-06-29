5aa64c594299cddcd3c60e1a9c76a0111bf84593
5hada <81249902+5hada@users.noreply.github.com>
2026-06-29 16:41:36 +0900

    feat: submodule
    add media control submodule

---
+++ src/sender/main.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/turing-smart-screen-python')
+
+# Import only the modules for LCD communication
+from library.lcd.lcd_comm import Orientation
+from library.lcd.lcd_comm_rev_a import LcdCommRevA
+from library.lcd.lcd_comm_rev_b import LcdCommRevB
+from library.lcd.lcd_comm_rev_c import LcdCommRevC
+from library.lcd.lcd_comm_rev_d import LcdCommRevD
